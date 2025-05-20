9cec0eaa3d64178c2327bb2c5735074f95fdfc3c
gabrielly reis <gabyreis88@gamil.com>
2025-05-19 22:49:59 -0300

    Remove comentários desnecessários e simplifica o código em index.html e script.js

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 document
   .getElementById("formContato")
   .addEventListener("submit", function (event) {
-    event.preventDefault(); // Evita o recarregamento da página
-
-    // Captura os valores dos campos
+    event.preventDefault();
     const nome = document.getElementById("nome").value;
     const email = document.getElementById("email").value;
     const mensagem = document.getElementById("mensagem").value;
-
-    // Validação simples
     if (nome === "" || email === "" || mensagem === "") {
       alert("Por favor, preencha todos os campos!");
       return;
     }
-
-    // Envia para o Google Apps Script
     const formData = { nome, email, mensagem };
     fetch(
       "https://script.google.com/macros/s/AKfycbw4W7u17JxLuiVAqezX_LFqg2L2fEu0D9GphOVqkh18w8K7ncVa21zXgx26M879wkWmSQ/exec",
